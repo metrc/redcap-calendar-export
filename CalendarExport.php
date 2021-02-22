f25971c544e406875bd537daf4b08f0236abd9ad
Paige Sullivan <psulli26@jhu.edu>
2021-02-22 13:11:31 -0500

    added user DAG checking and dropdowns to change month & year

--- CalendarExport.php
+++ CalendarExport.php
@@ -13,8 +13,7 @@ class CalendarExport extends AbstractExternalModule {
     }
 
     static function getEvent($cal_id) {
-        return db_fetch_assoc(db_query(sprintf('SELECT * FROM redcap_events_calendar WHERE cal_id = %d',
-            $cal_id)));
+        return db_fetch_assoc(db_query(sprintf('SELECT * FROM redcap_events_calendar WHERE cal_id = %d', $cal_id)));
     }
 
 
@@ -35,11 +34,11 @@ class CalendarExport extends AbstractExternalModule {
 
         if ($eventResult['record']) {
             // this is an event that is connected to a record
-            $returnResults['string_description'] = sprintf('%s : %s for record %d [%s]', $returnResults['project_name'],
+            $returnResults['string_description'] = sprintf('%s - %s : %s for record %d [%s]', $eventResult['event_date'], $returnResults['project_name'],
                 $returnResults['event_description'], $eventResult['record'], $returnResults['group_name']);
         } else {
             // general project task
-            $returnResults['string_description'] = sprintf('%s - %s', $returnResults['project_name'], $returnResults['notes']);
+            $returnResults['string_description'] = sprintf('%s - %s : %s', $eventResult['event_date'], $returnResults['project_name'], $returnResults['notes']);
         }
 
 
@@ -52,5 +51,15 @@ class CalendarExport extends AbstractExternalModule {
         printf('<label><input type="checkbox" name="cal_id[]" value="%d">&nbsp;%s</label><br/>', $eventInfo['cal_id'], $eventInfo['string_description']);
     }
 
+    static function hasAccessToDAG($user, $project, $dag) {
+        $sql = sprintf('SELECT group_id FROM redcap_user_rights WHERE project_id = %d AND username = "%s"', $project, $user);
+        $queryResult = db_query($sql);
+        if (db_num_rows($queryResult) == 1) return true;    // user has access to all groups
+        $resultArray = db_fetch_assoc($queryResult);
+        foreach($resultArray as $result) {
+            if ($result['group_id'] == $dag) return true;
+        }
+        return false;
+    }
 
 }
--- index.php
+++ index.php
@@ -6,12 +6,6 @@ if ($_REQUEST['cal_id']) {
 }
 
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
-?>
-
-    <h4>Calendar Export to Outlook</h4>
-    Click events you want to export to Outlook as an .ics file
-    <hr/>
-<?php
 
 if (!isset($_GET['year'])) {
     $_GET['year'] = date("Y");
@@ -22,12 +16,42 @@ if (!isset($_GET['month'])) {
 
 $month = $_GET['month'];
 $year = $_GET['year'];
+?>
 
+    <h4>Calendar Export to Outlook</h4>
+    Click events you want to export to Outlook as an .ics file
+    <hr/>
+<form method="get">
+    <input type="hidden" name="id" value="<?php echo $_REQUEST['id'] ?>"/>
+    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
+    <input type="hidden" name="pid" value="<?php echo $_REQUEST['pid'] ?>"/>
+
+    Month: <select name="month">
+        <?php
+            for($i = 1; $i <= 12; $i++) {
+                $selected = ($i == $month) ? ' selected="selected"' : null;
+                printf('<option value="%d"%s>%1$d</option>', $i, $selected);
+            }
+        ?>
+    </select>  Year: <select name="year">
+        <?php
+        for($i = 2021; $i <= 2046; $i++) {
+            $selected = ($i == $year) ? ' selected="selected"' : null;
+            printf('<option value="%d"%s>%1$d</option>', $i, $selected);
+        }
+        ?>
+    </select>
+    <button type="submit">Update Events</button>
+</form>
+
+
+<hr/>
+<?php
 $results = \JHU\CalendarExport\CalendarExport::getEvents($month, $year, $_REQUEST['pid']);
 print('<form method="post">');
 while($result = db_fetch_assoc($results)) {
-    \JHU\CalendarExport\CalendarExport::generateCheckbox($result);
-
+    if (\JHU\CalendarExport\CalendarExport::hasAccessToDAG($_SESSION['username'], $_REQUEST['pid'], $result['project_id']))
+        \JHU\CalendarExport\CalendarExport::generateCheckbox($result);
 }
 print('<p><button type="submit">Download Events as ICS</button></p></form>');
 
